7106e75732a660eb07d049a2957f180ca6bd83b6
RileyLipinski <55461529+RileyLipinski@users.noreply.github.com>
2019-10-10 14:21:35 -0500

    testsql added

--- server.js
+++ server.js
@@ -21,8 +21,10 @@ var db = new sqlite3.Database(db_filename, sqlite3.OPEN_READONLY, (err) => {
     }
     else {
         console.log('Now connected to ' + db_filename);
+        TestSql();
     }
 });
+
 
 app.use(express.static(public_dir));
 
